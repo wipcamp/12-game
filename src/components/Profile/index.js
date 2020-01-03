be13447635caa4e7b6fe99ab1dd3823a368f443b
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-01-04 00:16:20 +0700

    [Update] fix error line liff

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,39 +1,50 @@
-import React, { Component } from "react";
-import profileService from "../../services/profileService";
-import Progressbar from "./Progressbar";
-import Character from "./Character"
-import styled from 'styled-components'
-import SideNavbar from './SideNavbar'
+import React, {
+  Component
+} from 'react';
+import profileService from '../../services/profileService';
+import Progressbar from './Progressbar';
+import Character from './Character';
+import styled from 'styled-components';
+import SideNavbar from './SideNavbar';
 
-const CenterComponent = styled.div`
+const CenterComponent = styled.div `
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-`
+`;
 const liff = window.liff;
 export default class Profile extends Component {
   state = {
     user_test_data: {},
     sample_id: 1,
     percentExp: 0,
-    user_level : 0,
-    user_str : 0,
-    user_dex : 0,
-    user_luk : 0
+    user_level: 0,
+    user_str: 0,
+    user_dex: 0,
+    user_luk: 0
   };
 
   componentDidMount() {
     // this.getProfileData(this.state.sample_id);
+    liff
+      .init({
+        liffId: '1653691835-vZ4GNK7z'
+      })
+      .then(async () => {
+        if (!liff.isLoggedIn()) {
+          liff.login();
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
     this.getProfileData(this.getProfile());
   }
 
-  getProfile(){
-    liff.init(async () => {
-      let getProfile = await liff.getProfile();
-      this.setState({
-        sample_id: getProfile.userId
-      });
+  getProfile() {
+    liff.getProfile().then(dataInfo => {
+      return dataInfo.userId
     });
   }
 
@@ -41,54 +52,80 @@ export default class Profile extends Component {
     let data = await profileService.getProfile(id);
     this.setState({
       user_test_data: data.data,
-      percentExp: (data.data.exp*data.data.maxExp)/100,
-      user_level : data.data.level,
-      user_str : data.data.str,
-      user_dex : data.data.dex,
-      user_luk : data.data.luk
+      percentExp: (data.data.exp * data.data.maxExp) / 100,
+      user_level: data.data.level,
+      user_str: data.data.str,
+      user_dex: data.data.dex,
+      user_luk: data.data.luk
     });
   }
 
   render() {
-    return (
-      <div className="container">
-        <SideNavbar/>
-        <CenterComponent>
-        <Progressbar
-          color="warning"
-          percent={this.state.percentExp}
-          level = {this.state.user_level}
-          status="Level"
-        />
-        user_id : {this.state.user_test_data.id}
-        <br />
-        team : {this.state.user_test_data.team}
-        <br />
-        energy : {this.state.user_test_data.energy}
-        <br />
-        max_energy : {this.state.user_test_data.maxEnergy}
-        <br />
-        <Character level={this.state.user_level}/>
-        <Progressbar
-          color="warning"
-          percent={this.state.user_str}
-          level = {this.state.user_str}
-          status="str"
-        />
-        <Progressbar
-          color="warning"
-          percent={this.state.user_dex}
-          level = {this.state.user_dex}
-          status="dex"
-        />
-        <Progressbar
-          color="warning"
-          percent={this.state.user_luk*10}
-          level = {this.state.user_luk}
-          status="luk"
-        />
-        </CenterComponent>
-      </div>
+    return ( <
+      div className = 'container' >
+      <
+      SideNavbar / >
+      <
+      CenterComponent >
+      <
+      Progressbar color = 'warning'
+      percent = {
+        this.state.percentExp
+      }
+      level = {
+        this.state.user_level
+      }
+      status = 'Level' /
+      >
+      user_id: {
+        this.state.user_test_data.id
+      } < br / >
+      team: {
+        this.state.user_test_data.team
+      } < br / >
+      energy: {
+        this.state.user_test_data.energy
+      } < br / >
+      max_energy: {
+        this.state.user_test_data.maxEnergy
+      } < br / >
+      <
+      Character level = {
+        this.state.user_level
+      }
+      />{' '} <
+      Progressbar color = 'warning'
+      percent = {
+        this.state.user_str
+      }
+      level = {
+        this.state.user_str
+      }
+      status = 'str' /
+      >
+      <
+      Progressbar color = 'warning'
+      percent = {
+        this.state.user_dex
+      }
+      level = {
+        this.state.user_dex
+      }
+      status = 'dex' /
+      >
+      <
+      Progressbar color = 'warning'
+      percent = {
+        this.state.user_luk * 10
+      }
+      level = {
+        this.state.user_luk
+      }
+      status = 'luk' /
+      >
+      <
+      /CenterComponent>{' '} <
+      /div>
     );
   }
 }
