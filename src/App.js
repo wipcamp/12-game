02882ec268d61d4a2ac04dfcc687b69bca2e8b71
fame <famedirtyshit@gmail.com>
2020-01-07 18:24:17 +0700

    [UPDATE] update routing login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Profile from './components/Profile/index'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import LoginGame from './LoginGame';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Profile/>
+      <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Profile/>
+        </Route>
+        <Route exact path='/login'>
+          <LoginGame/>
+        </Route>
+      </Switch>
+    </Router>
       </header>
     </div>
   );
