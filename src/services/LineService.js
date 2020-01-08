4f374b2400601fcc3366c4ae98ea6d29d988ef57
fame <famedirtyshit@gmail.com>
2020-01-08 13:51:00 +0700

    [UPDATE] fix parameter form in lineLogin() 12-line-service

--- src/services/LineService.js
+++ src/services/LineService.js
@@ -1,13 +1,13 @@
 import api from '../utils/apiLineService'
 // import Cookies from './CookieService'
 
-const profileService = {
+const lineService = {
     lineLogin : async (code) => {
-        let res = await api.get(`/authForGame/?code=${code}`)
+        let res = await api.get(`/authForGame?code=${code}`)
         return res;
     },
   
   }
   
-  export default profileService
+  export default lineService
 
