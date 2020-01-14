36d5e6b990321b756a16d4932b7a5e61035409ef
fame <famedirtyshit@gmail.com>
2020-01-14 16:59:30 +0700

    [UPDATE] change http to https game service

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,4 +1,4 @@
 window["runConfig"] = {
-  apiUrl: "http://game.service.freezer.wip.camp",
+  apiUrl: "https://game.service.freezer.wip.camp",
   lineApi: "https://line.service.freezer.wip.camp"
 };
