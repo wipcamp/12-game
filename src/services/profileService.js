d24c4dc50d566216b599b4a5de152cf15ed48eb2
TipsukonCheer <tipsukon.cheer@mail.kmutt.ac.th>
2020-01-18 00:53:05 +0700

    [UPDATE] test addEnergyAPI

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -10,8 +10,12 @@ const profileService = {
         let res = await api.post(`/useEnergy?id=${lineId}`)
         return res;
     },
-    setCooldownTime : async (lineId,remainTime) => {
+    /*setCooldownTime : async (lineId,remainTime) => {
         let res = await api.put(`/setCooldownEnergyTime?id=${lineId}&remainTime=${remainTime}`)
+        return res;
+    },*/
+    setCooldownTime : async (lineId) => {
+        let res = await api.put(`/setCooldownEnergyTime?id=${lineId}`)
         return res;
     },
     getCooldownTime : async (lineId) => {
