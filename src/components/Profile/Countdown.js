380eedc28ee4ccdedbdaaced9fb9112daea36ed8
TipsukonCheer <tipsukon.cheer@mail.kmutt.ac.th>
2020-01-22 12:36:53 +0700

    [UPDATE] fix interval bug when time = 999

--- src/components/Profile/Countdown.js
+++ src/components/Profile/Countdown.js
@@ -3,57 +3,60 @@ import styled from 'styled-components'
 
 export default class Countdown extends Component {
 
-    state = { minute:this.props.minute , second:this.props.second }
+  state = { minute: this.props.minute, second: this.props.second }
 
-    timer() {
-
+  timer() {
+    if (minute != 999 && second != 999) {
       this.setState({
         second: this.state.second - 1
       });
-  
-      if(this.state.second < 1){
+
+      if (this.state.second < 1) {
         this.setState({
-         minute: this.state.minute - 1,
-         second: 59
+          minute: this.state.minute - 1,
+          second: 59
         });
       }
       if (this.state.minute < 0) {
         this.setState({
-        second: 0 ,
-        minute :0
+          second: 0,
+          minute: 0
         });
         this.props.onTimeOut();
-        if(this.state.minute<0){
-            clearInterval(this.intervalId);
+        if (this.state.minute < 0) {
+          clearInterval(this.intervalId);
         }
       }
-    }
-  
-    componentDidMount() {
-      this.intervalId = setInterval(this.timer.bind(this), 1000);
-    }
-  
-    componentWillUnmount() {
+    }else{
       clearInterval(this.intervalId);
     }
+  }
 
-    componentWillReceiveProps(nextProps) {
-        const { minute } = this.props.minute;
-        const { second } = this.props.second;
-        if (nextProps.second !== second) {
-          this.setState({ second: nextProps.second});
-        }
-        if (nextProps.minute !== minute) {
-          this.setState({ minute: nextProps.minute });
-        }
-      }
+  componentDidMount() {
+    this.intervalId = setInterval(this.timer.bind(this), 1000);
+  }
 
-    render() {
-      const { second , minute} = this.state;
-      if(minute != 999 && second != 999){
-        return <div>{minute>9?minute:'0'+minute}:{second>9?second:'0'+second}</div>;
-      }else{
-        return null;
-      }
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { minute } = this.props.minute;
+    const { second } = this.props.second;
+    if (nextProps.second !== second) {
+      this.setState({ second: nextProps.second });
+    }
+    if (nextProps.minute !== minute) {
+      this.setState({ minute: nextProps.minute });
     }
   }
+
+  render() {
+    const { second, minute } = this.state;
+    if (minute != 999 && second != 999) {
+      return <div>{minute > 9 ? minute : '0' + minute}:{second > 9 ? second : '0' + second}</div>;
+    } else {
+      return null;
+    }
+  }
+}
