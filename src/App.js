3b1959524dfe380e079e653bb4f3116f714f3ccb
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-02-05 15:51:26 +0700

    [Update] dev stage can login by browser

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import LoginGame from './components/LineLogin/index'
 import Liff from './components/LiffLogin/index'
-import Profile from './components/Profile_Test/index'
+
 function App() {
   return (
     <div className="App">
-      <Profile />
-      {/* <header className="App-header">
+      <header className="App-header">
         <Router>
           <Switch>
             <Route exact path='/login'>
@@ -18,7 +17,7 @@ function App() {
             </Route>
           </Switch>
         </Router>
-      </header> */}
+      </header>
     </div>
   );
 }
