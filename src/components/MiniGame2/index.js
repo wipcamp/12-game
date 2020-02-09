ab24b1cf7055a854c3dd15b9ea2e8ded67f825a7
TipsukonCheer <tipsukon.cheer@mail.kmutt.ac.th>
2020-02-09 20:09:02 +0700

    [ADD] MiniGame2 component

---
+++ src/components/MiniGame2/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+const liff = window.liff;
+
+export default class MiniGame2 extends Component {
+
+  state = {
+    isLoad : false,
+  }
+
+  componentDidMount(){
+    // if(liff){
+      liff
+        .init({
+          liffId: '1653691835-vZ4GNK7z'
+      })
+      .then(async () => {
+        if(!liff.isLoggedIn()){
+          console.log("not liff")
+          //รอpathเกมน้อง
+          // window.location.replace("/");
+        } else {
+            this.setState({
+              isLoad:false
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    });
+    // }
+  }
+
+  render() {
+      console.log(this.state.isLoad)
+      console.log("in minigame 2 in user")
+      console.log(liff)
+      if(this.state.isLoad){
+          return <p>loading</p>
+      }else{       
+        return (
+          <div>
+              <h1>minigame 2 page</h1>
+          </div>
+        );
+      }
+    }  
+}
