2a5d568c9d8d9d3769574f020fc8eefab559196a
TipsukonCheer <tipsukon.cheer@mail.kmutt.ac.th>
2020-02-10 12:31:26 +0700

    [UPDATE] can attack if in red gage

--- src/components/MiniGame2/GageBar.js
+++ src/components/MiniGame2/GageBar.js
@@ -10,6 +10,7 @@ border-right: 10px solid transparent;
 border-top: 20px solid red;
 position: relative;
 animation: mymove 2s infinite linear;
+animation-delay: 2s;
 
 @keyframes mymove {
     0%   {
@@ -32,21 +33,37 @@ export default class GageBar extends Component {
 
     state = {
         isLoad: false,
+        startTime: new Date(),
     }
 
     componentDidMount() {
+        
+    }
 
+    attack(time){
+        console.log((time-this.state.startTime)%1000)
+        let diff = (time-this.state.startTime)%1000
+        //ถ้าสีแดง=20
+        // if(diff>=700&&diff<=999){
+        //     console.log("diff"+diff)
+        // }
+        //ถ้าสีแดง=40
+        if((diff>=700&&diff<=999)||(diff>=1&&diff<=150)){
+            console.log("diff"+diff)
+        }
     }
 
     render() {
+        console.log(this.state.startTime)
         return (
             <GageSize>
                 <Triangle />
                 <Progress multi>
-                    <Progress bar color="warning" value="40" />
-                    <Progress bar color="danger" value="20" />
-                    <Progress bar color="warning" value="40" />
+                    <Progress bar color="warning" value="30" />
+                    <Progress bar color="danger" value="40" />
+                    <Progress bar color="warning" value="30" />
                 </Progress>
+                <button onClick={()=>this.attack(new Date())}>click</button>
             </GageSize>
         );
     }
