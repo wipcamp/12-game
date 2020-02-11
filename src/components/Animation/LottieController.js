0a88d1dd4cc29fd739d761f435ca5d9ffb558c0e
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-02-12 03:21:31 +0700

    [Update] format code

--- src/components/Animation/LottieController.js
+++ src/components/Animation/LottieController.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react'
 import Lottie from 'react-lottie'
 import animationData1 from '../../dist/animation/15260-pushups.json'
 import animationData2 from '../../dist/animation/15307-karaoke.json'
-
+import styled from 'styled-components'
 
 const buttonStyle = {
-    display: 'block',
-    margin: '10px auto'
+    position: 'relative',
+    zIndex: '1',
+    margin: '3em auto 0 auto',
+
 }
+
+const Round = styled.div`
+    position: relative;
+    z-index: 0.5;
+    width: 10em;
+    height: 5em;
+    border-radius: 50%;
+    background-color: #f00000
+    margin: 0 auto;
+    top: -2.5rem;
+
+`
 
 
 export default class LottieController extends Component {
@@ -48,20 +62,13 @@ export default class LottieController extends Component {
             }
         }
         
-        console.log(Math.floor(15/10))
-        console.log(defaultOptions.animationData)
-
         return (
             <React.Fragment>
                 <Lottie options={defaultOptions}
-                height={100}
+                height={200}
                 width={100} 
                 style={buttonStyle}/>
-                {/* // isStopped={this.state.isStopped}
-                // isPause={this.state.isPause} /> */}
-                {/* <button style={buttonStyle} onClick={this.handleAnimationData.bind(this)}>Change</button> */}
-                <button style={buttonStyle} onClick={() => this.setState({isStopped: false})}>Play</button>
-                <button style={buttonStyle} onClick={() => this.setState({isStopped: !this.state.isPause})}>Pause</button>
+                <Round />
             </React.Fragment>
         )
     }
