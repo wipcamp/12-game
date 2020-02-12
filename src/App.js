a3d6929aba97af3a12f3fd73430873f7dcb8fbfe
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-02-12 17:49:04 +0700

    [Update] for test

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import LoginGame from './components/LineLogin/index'
-import Liff from './components/LiffLogin/index'
-import Lottie from './components/Animation/index'
-import Profile from './components/Profile_Test/index'
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LoginGame from './components/LineLogin/index';
+import Liff from './components/LiffLogin/index';
+import Lottie from './components/Animation/index';
+import Profile from './components/Profile_Test/index';
+import Map from './components/Map/index';
 
 function App() {
   return (
-    <div className="App">
-      <Profile/ >
-      {/* <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <Router>
-          <Switch>
-            <Route exact path='/login'>
-              <LoginGame />
-            </Route>
-            <Route exact path='/'>
-              <Liff/>
-            </Route>
-          </Switch>
+          <Route exact path='/map'>
+            <Map />
+          </Route>
+          <Route exact path='/'>
+            <Profile />
+          </Route>
         </Router>
-      </header> */}
+      </header>
+      {/* <Profile/ > */}
+      {/* <header className="App-header">
+              <Router>
+                <Switch>
+                  <Route exact path='/login'>
+                    <LoginGame />
+                  </Route>
+                  <Route exact path='/'>
+                    <Liff/>
+                  </Route>
+                </Switch>
+              </Router>
+            </header> */}{' '}
     </div>
   );
 }
