b47000698c788ba52390db822e075e250eecf811
TipsukonCheer <tipsukon.cheer@mail.kmutt.ac.th>
2020-02-19 17:17:48 +0700

    [ADD] move Countdown component from profile to profile test

---
+++ src/components/Profile_Test/Countdown.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+export default class Countdown extends Component {
+
+  state = { minute: this.props.minute, second: this.props.second }
+
+  timer() {
+    if (this.state.minute != 999 && this.state.second != 999) {
+      this.setState({
+        second: this.state.second - 1
+      });
+
+      if (this.state.second < 1) {
+        this.setState({
+          minute: this.state.minute - 1,
+          second: 59
+        });
+      }
+      if (this.state.minute < 0) {
+        this.setState({
+          second: 0,
+          minute: 0
+        });
+        this.props.onTimeOut();
+        if (this.state.minute < 0) {
+          clearInterval(this.intervalId);
+        }
+      }
+    }else{
+      clearInterval(this.intervalId);
+    }
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(this.timer.bind(this), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { minute } = this.props.minute;
+    const { second } = this.props.second;
+    if (nextProps.second !== second) {
+      this.setState({ second: nextProps.second });
+    }
+    if (nextProps.minute !== minute) {
+      this.setState({ minute: nextProps.minute });
+    }
+  }
+
+  render() {
+    const { second, minute } = this.state;
+    if (minute != 999 && second != 999) {
+      return <div>{minute > 9 ? minute : '0' + minute}:{second > 9 ? second : '0' + second}</div>;
+    } else {
+      return null;
+    }
+  }
+}
