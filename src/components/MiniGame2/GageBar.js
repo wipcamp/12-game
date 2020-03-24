633291a34d47fdbb585bfc1c3b902919d62fbc14
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-03-25 00:34:23 +0700

    [Update] attack function complete

--- src/components/MiniGame2/GageBar.js
+++ src/components/MiniGame2/GageBar.js
@@ -9,7 +9,7 @@ const Triangle = styled.div`
   border-right: 1vh solid transparent;
   border-top: 2vh solid red;
   position: relative;
-  animation: mymove 10s infinite linear;
+  animation: mymove 5s infinite linear;
 
   @keyframes mymove {
     0% {
@@ -23,83 +23,78 @@ const Triangle = styled.div`
     }
   }
 `;
+
+const Div = styled.div`
+  background-color: red;
+  width: auto;
+  border: 1px solid red;
+  padding-left: 1vw;
+  display: inline-block;
+`;
 export default class GageBar extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.selector = React.createRef();
+    this.atTarget = React.createRef();
+  }
+
   state = {
     isLoad: false,
     startTime: new Date(),
     position: 30,
-    size: 10
+    size: 10,
+    point: 0
   };
 
-  time = {
-    //time init
-    init: 0,
-    //time start position target
-    start: 0,
-    //time end position target
-    end: 0,
-    //go or back
-    goBack: 0
-  };
-
-  componentDidMount() {}
+  componentDidMount() { }
 
   getRandom = (min, max) => {
     return Math.floor(min + Math.random() * (max + 1 - min));
   };
 
-  initTime = () => {
-    setInterval(() => {
-      let times = new Date().getMilliseconds();
-      this.time = {
-        init: times + 400,
-        //t: s * v
-        start: this.state.position * 10 + this.time.init,
-        end: this.state.size * 10 + this.time.start,
-        goBack: 1
-      };
-    }, 1000);
-  };
-
-  atk = t => {
-    let check = t + this.time.init;
-    if (check <= this.time.start || check >= this.time.end) {
+  attack = () => {
+    let rectangle = this.atTarget.current.getBoundingClientRect()
+    let mark = this.selector.current.getBoundingClientRect()
+    let begin = (this.state.position * Math.floor(rectangle.width))/100
+    let end = ((this.state.position + this.state.size) * Math.floor(rectangle.width))/100
+    if(mark.x >= begin && mark.x <= end){
+      console.log('hit')
       this.setState({
-        position: this.getRandom(10, 50)
-      });
-      console.log('hit');
-    } else {
-      console.log('missed');
+        position: this.getRandom(10, 50),
+        point: this.state.point+10
+      })
+    }else{
+      console.log('miss')
     }
-  };
+  }
 
   render() {
-    this.initTime()
     return (
       <div>
-        <Triangle />
-        <Progress
-          multi
-          style={{
-            marginTop: 10,
-            height: '2vh',
-            paddingVertical: 20,
-            width: '50vw'
-          }}
-        >
-          <Progress bar color='warning' value={this.state.position} />
-          <Progress bar color='danger' value={this.state.size} />
+        <p> point: {this.state.point} </p>
+        <Triangle ref={this.selector} />
+        <Div ref={this.atTarget}>
           <Progress
-            bar
-            color='warning'
-            value={100 - (this.state.size + this.state.position)}
-          />
-        </Progress>
-        <button onClick={() => this.atk(new Date().getMilliseconds())}>
+            multi
+            style={{
+              marginTop: 10,
+              height: '2vh',
+              paddingVertical: 20,
+              width: '50vw'
+            }}
+          >
+            <Progress bar color='warning' value={this.state.position} />
+            <Progress bar color='danger' value={this.state.size} />
+            <Progress
+              bar
+              color='warning'
+              value={100 - (this.state.size + this.state.position)}
+            />
+          </Progress>
+        </Div>
+        <button onClick={() => this.attack()}>
           Attack
         </button>
-        
       </div>
     );
   }
