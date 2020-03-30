fa2f77a2873b2a5cdc57311b6c4cf68fbf727bc2
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-03-30 15:33:30 +0700

    [Update] disable firebase temporarily

--- src/components/Realtime/firebase.js
+++ src/components/Realtime/firebase.js
@@ -1,3 +1,10 @@
 export const firebaseConfig = {
-  
+    // apiKey: 'AIzaSyDFXHJV71EStlZ6Jd16tI5brf4nGj9PUNg',
+    // authDomain: 'testforgame-851d2.firebaseapp.com',
+    // databaseURL: 'https://testforgame-851d2.firebaseio.com',
+    // projectId: 'testforgame-851d2',
+    // storageBucket: 'testforgame-851d2.appspot.com',
+    // messagingSenderId: '864964835557',
+    // appId: '1:864964835557:web:b9416399888d9373affc0c',
+    // measurementId: 'G-MCSBQZS62D'
 };
